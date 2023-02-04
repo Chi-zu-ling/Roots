2104ed2779f76e82163b19918fd0f83940a9d30d
Chi-zu-ling <haydenleete@gmail.com>
2023-02-04 15:26:47 +1300

    Add connections WIP

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection : MonoBehaviour
+{
+	public Node node1;
+	public Node node2;
+
+	LineRenderer line;
+
+	public Node GetOtherNode(Node node)
+	{
+		if (node == node1)
+		{
+			return node2;
+		} else
+		{
+			return node1;
+		}
+	}
+
+	private void Start()
+	{
+		line = GetComponent<LineRenderer>();
+		transform.position = 0.5f * (node1.transform.position + node2.transform.position);
+		line.SetPosition(0, node1.transform.position);
+		line.SetPosition(1, node2.transform.position);
+	}
+}
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Node : MonoBehaviour
 {
-
+    
     [SerializeField] Gamelogic gamelogic;
-    public List<Node> connectedNodes;
+    [SerializeField] GameObject connectionPrefab;
+    public List<Node> connectedNodes = new();
+    public List<Connection> connections = new();
     Vector2 position;
-    int cost;
+    public int cost;
+    float connectionRadius = 6;
+    float maxConnections = 10;
 
     public CircleCollider2D Connections;
 
@@ -27,22 +32,38 @@ public class Node : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
-
+        
     }
 
-    public void checkForConnections() {
+	private void OnMouseDown()
+	{
+        
+	}
 
-        Collider2D[] hits = (Physics2D.OverlapCircleAll(this.transform.position, 6)); //6 being connectionsRadius
+	public void checkForConnections() {
+
+        Collider2D[] hits = (Physics2D.OverlapCircleAll(this.transform.position, connectionRadius));
+        foreach(var hit in hits)
+		{
+            Node node = hit.GetComponent<Node>();
+            if (node != null || connectedNodes.Contains(node))
+			{
+                connectTo(node);
+			}
+		}
         //check for a radius around node
         //get all nodes available ("possibleConnections")
         //get random value between 1 and "maxConnections"
         //get random nodes from "possibleConnections"
         //make connection indicators?
-
     }
 
-    public void connectTo() {
+    public void connectTo(Node node) {
+        var connectionGo = Instantiate(connectionPrefab);
+        var connection = connectionGo.GetComponent<Connection>();
+        connection.node1 = this;
+        connection.node2 = node;
+        connections.Add(connection);
         //is called when pressed on, deducts cost from gamelogic.turns
     }
 
